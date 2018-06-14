afb1930ae87fe880924a58719898ced3bad06d50
Mattias Svensson <mattias.svensson@gpsgate.com>
2018-06-14 12:16:48 +0200

    Added Lerna for managing npm publish.

--- packages/react-components/webpack.config.js
+++ packages/react-components/webpack.config.js
@@ -2,6 +2,7 @@ module.exports = {
   name: "components", // TODO: react-components?
   context: "src",
   entry: "./index.js",
+  outputPath: "dist",
   publicPath: "../GpsGateOnlineScripts/Webpack/vehicletracker/webpack_dist/", // TODO: not needed for this lib?
   library: "GpsGateComponents" // TODO: GpsGateReactComponents?
 };
